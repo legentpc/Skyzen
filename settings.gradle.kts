legentpc/Chatter

Added Support 26.1.X with stonecutter fabric in kotlin

settings.gradle.kts
@@ -1,0 +1,32 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.fabricmc.net/") { name = "Fabric" }
+        maven("https://maven.kikugie.dev/releases") { name = "KikuGie Releases" }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("dev.kikugie.stonecutter") version "0.9.8"
+}
+
+rootProject.name = "chatter"
+
+stonecutter {
+    create(rootProject) {
+        version("26.1", "26.1.2")
+    }
+}
+
+val supportedVersions = listOf("26.1")
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        supportedVersions.forEach {
+            create("versionedLibs${it.replace(".", "")}") {
+                from(files("gradle/${it.replace(".", "_")}.versions.toml"))
+            }
+        }
+    }
+}

src/main/fabric/at/legentpc/chatter/config/ConfigGuiManager.kt
@@ -1,0 +1,16 @@
+package at.legentpc.chatter.config
+
+import at.legentpc.chatter.ChatterModLoader
+import io.github.notenoughupdates.moulconfig.common.IMinecraft
+import net.minecraft.client.Minecraft
+
+object ConfigGuiManager {
+
+    fun openGui() {
+        val client = Minecraft.getInstance()
+        client.execute {
+            val editor = ChatterModLoader.configManager.managedConfig.getEditor()
+            IMinecraft.INSTANCE.openWrappedScreen(editor)
+        }
+    }
+}

src/main/fabric/at/legentpc/chatter/config/features/misc/MiscConfig.kt
@@ -1,0 +1,11 @@
+package at.legentpc.chatter.config.features.misc
+
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean
+import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
+
+class MiscConfig {
+
+    @ConfigOption(name = "Hide Gift Nametag", desc = "Hides the nametag above gift entities in Skyblock")
+    @ConfigEditorBoolean
+    var hideGiftNametag: Boolean = true
+}

src/main/fabric/at/legentpc/chatter/events/IslandChangeEvent.kt
@@ -1,0 +1,7 @@
+package at.legentpc.chatter.events
+
+import at.legentpc.chatter.utils.IslandType
+
+fun interface IslandChangeEvent {
+    fun onIslandChange(newIsland: IslandType, oldIsland: IslandType)
+}

src/main/fabric/at/legentpc/chatter/events/WorldChangeEvent.kt
@@ -1,0 +1,20 @@
+package at.legentpc.chatter.events
+
+/**
+ * Fired after the client switched to a different world, meaning the [net.minecraft.client.multiplayer.ClientLevel]
+ * instance was replaced (server switch, world join), or the SkyBlock sidebar disappeared (disconnect).
+ *
+ * Fired on the main client thread, primarily via the Fabric `ClientLevelEvents.AFTER_CLIENT_LEVEL_CHANGE`
+ * callback. On Hypixel every server switch replaces the world, so this fires on every island change as well.
+ *
+ * This is the standard event for resetting world bound state, for example cached entities, positions or timers.
+ *
+ * Do not use this when the concrete island matters. This event carries no [at.legentpc.chatter.utils.IslandType]
+ * and also fires for world changes that are not island changes. Use [IslandChangeEvent] instead, which reports
+ * both the old and the new island.
+ *
+ * @see IslandChangeEvent
+ */
+fun interface WorldChangeEvent {
+    fun onWorldChange()
+}

src/main/fabric/at/legentpc/chatter/module/Module.kt
@@ -1,0 +1,6 @@
+package at.legentpc.chatter.module
+
+abstract class Module(
+    val name: String,
+    val needsToggle: Boolean = true
+)

src/main/fabric/at/legentpc/chatter/utils/Data.kt
@@ -1,0 +1,9 @@
+package at.legentpc.chatter.utils
+
+import com.google.gson.annotations.Expose
+import java.util.UUID
+
+data class Data(
+    @Expose val devs: List<UUID> = emptyList(),
+    @Expose val emojiName: Map<UUID, String> = emptyMap()
+)

src/main/fabric/at/legentpc/chatter/utils/HypixelUtils.kt
@@ -1,0 +1,68 @@
+package at.legentpc.chatter.utils
+
+import at.legentpc.chatter.events.ChatterEvents
+import at.legentpc.chatter.module.Module
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLevelEvents
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
+import net.minecraft.client.Minecraft
+import net.minecraft.world.scores.DisplaySlot
+
+object HypixelUtils : Module("HypixelUtils", needsToggle = false) {
+
+    var currentIsland: IslandType = IslandType.UNKNOWN
+        private set
+    var inSkyblock: Boolean = false
+        private set
+
+    private val colorCode = Regex("§.")
+    private var tickCounter = 0
+
+    fun init() {
+        ClientTickEvents.END_CLIENT_TICK.register {
+            if (++tickCounter % 10 == 0) onTick()
+        }
+        ClientLevelEvents.AFTER_CLIENT_LEVEL_CHANGE.register { _, _ ->
+            onWorldChange()
+        }
+    }
+
+    private fun onTick() {
+        val scoreboard = Minecraft.getInstance().level?.scoreboard
+        val objective = scoreboard?.getDisplayObjective(DisplaySlot.SIDEBAR)
+
+        if (scoreboard == null || objective == null) {
+            if (inSkyblock) onWorldChange()
+            return
+        }
+
+        val wasInSkyblock = inSkyblock
+        inSkyblock = objective.displayName.string.contains("SKYBLOCK", ignoreCase = true)
+
+        if (!inSkyblock) {
+            if (wasInSkyblock) onWorldChange()
+            return
+        }
+
+        val lines = scoreboard.listPlayerScores(objective)
+            .map { it.owner() }
+
+        val areaLine = lines.firstOrNull { it.contains("⏣") || it.contains("ф") }
+        if (areaLine != null) {
+            val cleaned = areaLine.replace(colorCode, "").replace("⏣", "").replace("ф", "").trim()
+            updateIsland(IslandType.fromArea(cleaned))
+        }
+    }
+
+    private fun onWorldChange() {
+        inSkyblock = false
+        updateIsland(IslandType.UNKNOWN)
+        ChatterEvents.WORLD_CHANGE.invoker().onWorldChange()
+    }
+
+    private fun updateIsland(newIsland: IslandType) {
+        if (newIsland == currentIsland) return
+        val oldIsland = currentIsland
+        currentIsland = newIsland
+        ChatterEvents.ISLAND_CHANGE.invoker().onIslandChange(newIsland, oldIsland)
+    }
+}

legentpc/Skyzen

settings.gradle.kts
@@ -1,0 +1,32 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.fabricmc.net/") { name = "Fabric" }
+        maven("https://maven.kikugie.dev/releases") { name = "KikuGie Releases" }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("dev.kikugie.stonecutter") version "0.9.8"
+}
+
+rootProject.name = "skyzen"
+
+stonecutter {
+    create(rootProject) {
+        version("26.1", "26.1.2")
+    }
+}
+
+val supportedVersions = listOf("26.1")
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        supportedVersions.forEach {
+            create("versionedLibs${it.replace(".", "")}") {
+                from(files("gradle/${it.replace(".", "_")}.versions.toml"))
+            }
+        }
+    }
+}

src/main/fabric/at/legentpc/skyzen/config/ConfigGuiManager.kt
@@ -1,0 +1,16 @@
+package at.legentpc.skyzen.config
+
+import at.legentpc.skyzen.SkyzenModLoader
+import io.github.notenoughupdates.moulconfig.common.IMinecraft
+import net.minecraft.client.Minecraft
+
+object ConfigGuiManager {
+
+    fun openGui() {
+        val client = Minecraft.getInstance()
+        client.execute {
+            val editor = SkyzenModLoader.configManager.managedConfig.getEditor()
+            IMinecraft.INSTANCE.openWrappedScreen(editor)
+        }
+    }
+}

src/main/fabric/at/legentpc/skyzen/config/features/misc/MiscConfig.kt
@@ -1,0 +1,11 @@
+package at.legentpc.skyzen.config.features.misc
+
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean
+import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
+
+class MiscConfig {
+
+    @ConfigOption(name = "Hide Gift Nametag", desc = "Hides the nametag above gift entities in Skyblock")
+    @ConfigEditorBoolean
+    var hideGiftNametag: Boolean = true
+}

src/main/fabric/at/legentpc/skyzen/events/IslandChangeEvent.kt
@@ -1,0 +1,7 @@
+package at.legentpc.skyzen.events
+
+import at.legentpc.skyzen.utils.IslandType
+
+fun interface IslandChangeEvent {
+    fun onIslandChange(newIsland: IslandType, oldIsland: IslandType)
+}

src/main/fabric/at/legentpc/skyzen/events/WorldChangeEvent.kt
@@ -1,0 +1,20 @@
+package at.legentpc.skyzen.events
+
+/**
+ * Fired after the client switched to a different world, meaning the [net.minecraft.client.multiplayer.ClientLevel]
+ * instance was replaced (server switch, world join), or the SkyBlock sidebar disappeared (disconnect).
+ *
+ * Fired on the main client thread, primarily via the Fabric `ClientLevelEvents.AFTER_CLIENT_LEVEL_CHANGE`
+ * callback. On Hypixel every server switch replaces the world, so this fires on every island change as well.
+ *
+ * This is the standard event for resetting world bound state, for example cached entities, positions or timers.
+ *
+ * Do not use this when the concrete island matters. This event carries no [at.legentpc.skyzen.utils.IslandType]
+ * and also fires for world changes that are not island changes. Use [IslandChangeEvent] instead, which reports
+ * both the old and the new island.
+ *
+ * @see IslandChangeEvent
+ */
+fun interface WorldChangeEvent {
+    fun onWorldChange()
+}

src/main/fabric/at/legentpc/skyzen/module/Module.kt
@@ -1,0 +1,6 @@
+package at.legentpc.skyzen.module
+
+abstract class Module(
+    val name: String,
+    val needsToggle: Boolean = true
+)

src/main/fabric/at/legentpc/skyzen/utils/Data.kt
@@ -1,0 +1,9 @@
+package at.legentpc.skyzen.utils
+
+import com.google.gson.annotations.Expose
+import java.util.UUID
+
+data class Data(
+    @Expose val devs: List<UUID> = emptyList(),
+    @Expose val emojiName: Map<UUID, String> = emptyMap()
+)

src/main/fabric/at/legentpc/skyzen/utils/HypixelUtils.kt
@@ -1,0 +1,68 @@
+package at.legentpc.skyzen.utils
+
+import at.legentpc.skyzen.events.SkyzenEvents
+import at.legentpc.skyzen.module.Module
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLevelEvents
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
+import net.minecraft.client.Minecraft
+import net.minecraft.world.scores.DisplaySlot
+
+object HypixelUtils : Module("HypixelUtils", needsToggle = false) {
+
+    var currentIsland: IslandType = IslandType.UNKNOWN
+        private set
+    var inSkyblock: Boolean = false
+        private set
+
+    private val colorCode = Regex("§.")
+    private var tickCounter = 0
+
+    fun init() {
+        ClientTickEvents.END_CLIENT_TICK.register {
+            if (++tickCounter % 10 == 0) onTick()
+        }
+        ClientLevelEvents.AFTER_CLIENT_LEVEL_CHANGE.register { _, _ ->
+            onWorldChange()
+        }
+    }
+
+    private fun onTick() {
+        val scoreboard = Minecraft.getInstance().level?.scoreboard
+        val objective = scoreboard?.getDisplayObjective(DisplaySlot.SIDEBAR)
+
+        if (scoreboard == null || objective == null) {
+            if (inSkyblock) onWorldChange()
+            return
+        }
+
+        val wasInSkyblock = inSkyblock
+        inSkyblock = objective.displayName.string.contains("SKYBLOCK", ignoreCase = true)
+
+        if (!inSkyblock) {
+            if (wasInSkyblock) onWorldChange()
+            return
+        }
+
+        val lines = scoreboard.listPlayerScores(objective)
+            .map { it.owner() }
+
+        val areaLine = lines.firstOrNull { it.contains("⏣") || it.contains("ф") }
+        if (areaLine != null) {
+            val cleaned = areaLine.replace(colorCode, "").replace("⏣", "").replace("ф", "").trim()
+            updateIsland(IslandType.fromArea(cleaned))
+        }
+    }
+
+    private fun onWorldChange() {
+        inSkyblock = false
+        updateIsland(IslandType.UNKNOWN)
+        SkyzenEvents.WORLD_CHANGE.invoker().onWorldChange()
+    }
+
+    private fun updateIsland(newIsland: IslandType) {
+        if (newIsland == currentIsland) return
+        val oldIsland = currentIsland
+        currentIsland = newIsland
+        SkyzenEvents.ISLAND_CHANGE.invoker().onIslandChange(newIsland, oldIsland)
+    }
+}