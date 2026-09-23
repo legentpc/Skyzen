655048a01efd976bf582e107efe8113321d7950f
legentpc <jk00gaming00@gmail.com>
2026-09-23 13:49:37 +0530

    data kt deleted by me

--- src/main/fabric/at/legentpc/skyzen/utils/Data.kt
+++
@@ -1,9 +0,0 @@
-package at.legentpc.skyzen.utils
-
-import com.google.gson.annotations.Expose
-import java.util.UUID
-
-data class Data(
-    @Expose val devs: List<UUID> = emptyList(),
-    @Expose val emojiName: Map<UUID, String> = emptyMap()
-)
